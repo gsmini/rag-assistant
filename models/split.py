a1bce8ff1101cb8a8ed8d6c9577a6d470d511612
xiongyao <gsmini@sina.cn>
2025-04-09 12:44:01 +0800

    feat text split

--- models/split.py
+++ models/split.py
@@ -1,31 +1,37 @@
+import pprint
+
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 """
 文本分块函数
+@content: 输入的待分块文本
+@return: 返回分块后的文本列表
 """
 
 
-def text_splitter(docs=None):
-    if docs is None:
-        docs = []
+def text_splitter(content=None):
+    if content is None:
+        content = ""
 
-    doc_data = []
-    doc_spliter = RecursiveCharacterTextSplitter(chunk_size=128,  # 单个文件块128长度
-                                                 chunk_overlap=30,  # 允许重复内容为30长度
+    split_data = []
+    spliter = RecursiveCharacterTextSplitter(chunk_size=128,  # 单个文件块128长度
+                                             chunk_overlap=30,  # 允许重复内容为30长度
 
-                                                 separators=["\n\n",
-                                                             "\n",
-                                                             ".",
-                                                             "\uff0e",  # Fullwidth full stop
-                                                             "\u3002",  # Ideographic full stop
-                                                             ",",
-                                                             "\uff0c",  # Fullwidth comma
-                                                             "\u3001",  # Ideographic comma
-                                                             ])
-    for data in docs:
-        content = data["content_with_weight"]
-        if '<table>' not in content and len(content) > 200:
-            doc_data = doc_data + doc_spliter.split_text(content)
-        else:
-            doc_data.append(content)
-    return doc_data
+                                             separators=["\n\n",
+                                                         "\n",
+                                                         ".",
+                                                         "\uff0e",  # 中文句号。
+                                                         "\u3002",  # 中文句号。
+                                                         ",",
+                                                         "\uff0c",  # 中文，
+                                                         "\u3001",  # 中文、
+                                                         ])
+    if '<table>' not in content and len(content) > 200:
+        split_data = split_data + spliter.split_text(content)
+    else:
+        split_data.append(content)
+    return split_data
+
+
+if __name__ == "__main__":
+    pass
