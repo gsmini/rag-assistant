9266db4fd8dd3e92b717677b1b7bd53791382a4a
xiongyao <gsmini@sina.cn>
2025-04-09 12:43:32 +0800

    feat bge-m3 embedding

--- models/embedding.py
+++ models/embedding.py
@@ -1,20 +1,28 @@
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from config import device
+from config import device, BASE_DIR
+# from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 """
 向量化文本
 """
 
 
-class RagEmbedding:
-    def __init__(self, model_path="third_party/embeddings_models/bge-m3/",
+class RagBgeM3Embedding:
+    def __init__(self,
+                 model_path=BASE_DIR + "/third_party/embeddings_models/bge-m3/",
                  device=device):
+        encode_kwargs = {'normalize_embeddings': True}  # 标准归一化
+
         self.embedding = HuggingFaceEmbeddings(model_name=model_path,
-                                               model_kwargs={"device": device})
+                                               model_kwargs={"device": device},
+                                               encode_kwargs=encode_kwargs
+                                               )
 
     def get_embedding_fun(self):
         return self.embedding
 
 
-if __name__ == "__mian__":
+bgem3_embedding = RagBgeM3Embedding()
+
+if __name__ == "__main__":
     pass
