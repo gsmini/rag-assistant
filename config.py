ac93d74972206b0dd7e9d5aeb190e9e59b230a15
xiongyao <gsmini@sina.cn>
2025-04-09 12:44:40 +0800

    optimize config.py

--- config.py
+++ config.py
@@ -2,11 +2,16 @@ import environs
 import os
 
 env = environs.Env()
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 env.read_env(os.path.join(BASE_DIR, '.env'))
 
-llm_base_url = env.str('llm_base_url')
-llm_api_key = env.str('llm_api_key')
-chromadb_host = env.str('chromadb_host')
-chromadb_port = env.str('chromadb_port')
-device = env.str('device')
+llm_base_url = env.str('llm_base_url', "")
+llm_api_key = env.str('llm_api_key', "")
+chromadb_host = env.str('chromadb_host', "")
+chromadb_port = env.int('chromadb_port', 8080)
+device = env.str('device', "cpu")
+
+if __name__ == "__main__":
+    print(BASE_DIR+"/third_party/embeddings_models/bge-m3/")
+    print(chromadb_host)
+    print(chromadb_port)
